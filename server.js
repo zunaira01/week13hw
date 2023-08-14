75e74f46d74b34e22f598842cad8d4214ffedbd8
zunaira01 <134541019+zunaira01@users.noreply.github.com>
2023-08-14 10:35:14 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,60 +1,45 @@
-// app.js
+const express = require("express");
+// NOTE: it must start with. if it's just a file, not an NPM package
+const fruits =  require('./models/fruits.js');
+const vegatables = require('./models/vegatables.js');
 
-const express = require('express');
+
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Good Morning, Courtney!'));
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('jsx-view-engine').createEngine());
 
 
+app.get('/', (req, res) => {
+    res.send('homepage of fruits and vegatables');
+});
+//Define routes
+app.get("/fruits/", (req, res) => {
+  // res.send(fruits);
+  res.render('Index, {fruits: fruits}');
+});
 
+//Define routes
+app.get("/vegatables", (req, res) => {
+    res.send(vegatables);
+  });
+  
+//Createa a SHOW route (show routes use a get request)
+app.get("/fruits/:indexOfFruitsArray", (req, res) => {
+  // res.send(fruits[req.params.indexOfFruitsArray]);
+  res.render ('Show', {
+  // inculde a second param that must be an object (req.params,indexOfFruitsArray)
+  fruit: fruits[req.params.indexOfFruitsArray]
+  });
+});
 
+ //Createa a SHOW route (show routes use a get request)
+ app.get("/vegatables/:indexOfVegatablesArray", (req, res) => {
+    res.send(vegatables[req.params.indexOfVegatablesArray]);
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require("express");
-
-// const app = ("express");
-
-// const greet =  require('./Components/Greet.js');
-
-
-
-
-// app.get('/', (req, res) => {
-//     res.send('Welcome to our website!');
-// });
-
-
-// app.get("/greet/", (req, res) => {
-//   res.send(greet);
-// });
-
-//  app.get("/greet/:indexOfGreetArray", (req, res) => {
-//     res.send(greet[req.params.indexOfGreetArray]);
-//   });
-
-// app.listen(3000, () => {
-//   console.log("Listening on port 3000");
-// });
-
-
-
-
-
-
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
 
